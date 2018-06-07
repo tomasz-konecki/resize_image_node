f9011cb08bfa7f5288a308ac409ad6a4859fcf10
Konecki <koneckit@emea.roche.com>
2018-06-07 08:57:16 +0200

    Added code

--- test.js
+++ test.js
@@ -2,3 +2,4 @@ const chalk = require("chalk");
 
 console.log(chalk.red("Hello", chalk.underline("world") + "!"));
 console.log(chalk.underline.rgb(123, 45, 67)("Underlined reddish color"));
+console.log(chalk.underline.rgb(3, 145, 2)("Underlined greenish color"));
